8cac078ae531a6cd5b250d92643faebc30eb332d
refilwe5751 <refilwemoraba5@gmail.com>
2023-02-27 17:27:30 -0800

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the lowercase alphabet followed by a newline
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+  char letter = 'a';
+
+  while (letter <= 'z')
+    {
+      _putchar(letter);
+      letter++;
+    }
+
+  _putchar('\n');
+}
+
