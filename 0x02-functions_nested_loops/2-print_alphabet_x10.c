c3114dc8c7691bb526c54d3a68afce3cb04363f6
refilwe5751 <refilwemoraba5@gmail.com>
2023-02-27 17:45:28 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the lowercase alphabet 10 times followed by a newline
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+  char letter;
+  int i;
+
+  for (i = 0; i < 10; i++)
+    {
+      letter = 'a';
+
+      while (letter <= 'z')
+	{
+	  _putchar(letter);
+	  letter++;
+	}
+
+      _putchar('\n');
+    }
+}
+
